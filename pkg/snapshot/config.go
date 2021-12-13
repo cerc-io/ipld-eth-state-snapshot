ba8e32ce97415f8ec5d476fc2c0e6ded2d965d34
Arijit Das <arijitad.in@gmail.com>
2021-12-13 20:31:32 +0530

    Update geth and implement transaction batching.

--- pkg/snapshot/config.go
+++ pkg/snapshot/config.go
@@ -16,43 +16,69 @@
 package snapshot
 
 import (
+	"github.com/ethereum/go-ethereum/statediff/indexer/node"
+	"github.com/ethereum/go-ethereum/statediff/indexer/postgres"
 	"github.com/spf13/viper"
-
-	"github.com/vulcanize/ipfs-blockchain-watcher/pkg/config"
-	"github.com/vulcanize/ipfs-blockchain-watcher/pkg/core"
 )
 
 const (
-	ANCIENT_DB_PATH   = "ANCIENT_DB_PATH"
-	ETH_CLIENT_NAME   = "ETH_CLIENT_NAME"
-	ETH_GENESIS_BLOCK = "ETH_GENESIS_BLOCK"
-	ETH_NETWORK_ID    = "ETH_NETWORK_ID"
-	ETH_NODE_ID       = "ETH_NODE_ID"
-	LVL_DB_PATH       = "LVL_DB_PATH"
+	ancientDbPath   = "ANCIENT_DB_PATH"
+	ethClientName   = "ETH_CLIENT_NAME"
+	ethGenesisBlock = "ETH_GENESIS_BLOCK"
+	ethNetworkId    = "ETH_NETWORK_ID"
+	ethNodeId       = "ETH_NODE_ID"
+	lvlDbPath       = "LVL_DB_PATH"
 )
 
 type Config struct {
 	LevelDBPath   string
 	AncientDBPath string
-	Node          core.Node
-	DBConfig      config.Database
+	Node          node.Info
+	connectionURI string
+	DBConfig      postgres.ConnectionConfig
 }
 
 func (c *Config) Init() {
-	c.DBConfig.Init()
-	viper.BindEnv("leveldb.path", LVL_DB_PATH)
-	viper.BindEnv("ethereum.nodeID", ETH_NODE_ID)
-	viper.BindEnv("ethereum.clientName", ETH_CLIENT_NAME)
-	viper.BindEnv("ethereum.genesisBlock", ETH_GENESIS_BLOCK)
-	viper.BindEnv("ethereum.networkID", ETH_NETWORK_ID)
-	viper.BindEnv("leveldb.ancient", ANCIENT_DB_PATH)
+	c.dbInit()
+	viper.BindEnv("leveldb.path", lvlDbPath)
+	viper.BindEnv("ethereum.nodeID", ethNodeId)
+	viper.BindEnv("ethereum.clientName", ethClientName)
+	viper.BindEnv("ethereum.genesisBlock", ethGenesisBlock)
+	viper.BindEnv("ethereum.networkID", ethNetworkId)
+	viper.BindEnv("leveldb.ancient", ancientDbPath)
 
-	c.Node = core.Node{
+	c.Node = node.Info{
 		ID:           viper.GetString("ethereum.nodeID"),
 		ClientName:   viper.GetString("ethereum.clientName"),
 		GenesisBlock: viper.GetString("ethereum.genesisBlock"),
 		NetworkID:    viper.GetString("ethereum.networkID"),
+		ChainID:      viper.GetUint64("ethereum.chainID"),
 	}
 	c.LevelDBPath = viper.GetString("leveldb.path")
 	c.AncientDBPath = viper.GetString("leveldb.ancient")
+}
+
+func (c *Config) dbInit() {
+	viper.BindEnv("database.name", postgres.DATABASE_NAME)
+	viper.BindEnv("database.hostname", postgres.DATABASE_HOSTNAME)
+	viper.BindEnv("database.port", postgres.DATABASE_PORT)
+	viper.BindEnv("database.user", postgres.DATABASE_USER)
+	viper.BindEnv("database.password", postgres.DATABASE_PASSWORD)
+	viper.BindEnv("database.maxIdle", postgres.DATABASE_MAX_IDLE_CONNECTIONS)
+	viper.BindEnv("database.maxOpen", postgres.DATABASE_MAX_OPEN_CONNECTIONS)
+	viper.BindEnv("database.maxLifetime", postgres.DATABASE_MAX_CONN_LIFETIME)
+
+	dbParams := postgres.ConnectionParams{}
+	// DB params
+	dbParams.Name = viper.GetString("database.name")
+	dbParams.Hostname = viper.GetString("database.hostname")
+	dbParams.Port = viper.GetInt("database.port")
+	dbParams.User = viper.GetString("database.user")
+	dbParams.Password = viper.GetString("database.password")
+
+	c.connectionURI = postgres.DbConnectionString(dbParams)
+	// DB config
+	c.DBConfig.MaxIdle = viper.GetInt("database.maxIdle")
+	c.DBConfig.MaxOpen = viper.GetInt("database.maxOpen")
+	c.DBConfig.MaxLifetime = viper.GetInt("database.maxLifetime")
 }
--- pkg/snapshot/publisher.go
+++ pkg/snapshot/publisher.go
@@ -19,22 +19,25 @@ import (
 	"bytes"
 
 	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ipfs/go-ipfs-blockstore"
-	"github.com/ipfs/go-ipfs-ds-help"
+	blockstore "github.com/ipfs/go-ipfs-blockstore"
+	dshelp "github.com/ipfs/go-ipfs-ds-help"
+	"github.com/jmoiron/sqlx"
 	"github.com/multiformats/go-multihash"
 
-	"github.com/vulcanize/ipfs-blockchain-watcher/pkg/ipfs/ipld"
-	"github.com/vulcanize/ipfs-blockchain-watcher/pkg/postgres"
-	"github.com/vulcanize/ipfs-blockchain-watcher/pkg/shared"
+	"github.com/ethereum/go-ethereum/statediff/indexer/ipfs/ipld"
+	"github.com/ethereum/go-ethereum/statediff/indexer/postgres"
+	"github.com/ethereum/go-ethereum/statediff/indexer/shared"
 )
 
 type Publisher struct {
-	db *postgres.DB
+	db            *postgres.DB
+	currBatchSize uint
 }
 
 func NewPublisher(db *postgres.DB) *Publisher {
 	return &Publisher{
-		db: db,
+		db:            db,
+		currBatchSize: 0,
 	}
 }
 
@@ -44,10 +47,12 @@ func (p *Publisher) PublishHeader(header *types.Header) (int64, error) {
 	if err != nil {
 		return 0, err
 	}
+
 	tx, err := p.db.Beginx()
 	if err != nil {
 		return 0, err
 	}
+
 	defer func() {
 		if p := recover(); p != nil {
 			shared.Rollback(tx)
@@ -58,9 +63,11 @@ func (p *Publisher) PublishHeader(header *types.Header) (int64, error) {
 			err = tx.Commit()
 		}
 	}()
-	if err := shared.PublishIPLD(tx, headerNode); err != nil {
+
+	if err = shared.PublishIPLD(tx, headerNode); err != nil {
 		return 0, err
 	}
+
 	mhKey, _ := shared.MultihashKeyFromCIDString(headerNode.Cid().String())
 	var headerID int64
 	err = tx.QueryRowx(`INSERT INTO eth.header_cids (block_number, block_hash, parent_hash, cid, td, node_id, reward, state_root, tx_root, receipt_root, uncle_root, bloom, timestamp, mh_key, times_validated)
@@ -69,86 +76,107 @@ func (p *Publisher) PublishHeader(header *types.Header) (int64, error) {
  								RETURNING id`,
 		header.Number.Uint64(), header.Hash().Hex(), header.ParentHash.Hex(), headerNode.Cid().String(), "0", p.db.NodeID, "0", header.Root.Hex(), header.TxHash.Hex(),
 		header.ReceiptHash.Hex(), header.UncleHash.Hex(), header.Bloom.Bytes(), header.Time, mhKey, 0).Scan(&headerID)
+
 	return headerID, err
 }
 
 // PublishStateNode writes the state node to the ipfs backing datastore and adds secondary indexes in the state_cids table
-func (p *Publisher) PublishStateNode(node Node, headerID int64) (int64, error) {
+func (p *Publisher) PublishStateNode(node Node, headerID int64, tx *sqlx.Tx) (int64, error) {
 	var stateID int64
 	var stateKey string
 	if !bytes.Equal(node.Key.Bytes(), nullHash.Bytes()) {
 		stateKey = node.Key.Hex()
 	}
-	tx, err := p.db.Beginx()
-	if err != nil {
-		return 0, err
-	}
+
 	defer func() {
-		if p := recover(); p != nil {
+		if rec := recover(); rec != nil {
 			shared.Rollback(tx)
-			panic(p)
-		} else if err != nil {
-			shared.Rollback(tx)
-		} else {
-			err = tx.Commit()
+			panic(rec)
 		}
 	}()
-	stateCIDStr, err := shared.PublishRaw(tx, ipld.MEthStateTrie, multihash.KECCAK_256, node.Value)
+
+	stateCIDStr, mhKey, err := shared.PublishRaw(tx, ipld.MEthStateTrie, multihash.KECCAK_256, node.Value)
 	if err != nil {
 		return 0, err
 	}
-	mhKey, _ := shared.MultihashKeyFromCIDString(stateCIDStr)
+
 	err = tx.QueryRowx(`INSERT INTO eth.state_cids (header_id, state_leaf_key, cid, state_path, node_type, diff, mh_key) VALUES ($1, $2, $3, $4, $5, $6, $7)
- 									ON CONFLICT (header_id, state_path) DO UPDATE SET state_path = state_cids.state_path
+ 									ON CONFLICT (header_id, state_path) DO UPDATE SET (state_leaf_key, cid, node_type, diff, mh_key) = ($2, $3, $5, $6, $7)
  									RETURNING id`,
 		headerID, stateKey, stateCIDStr, node.Path, node.NodeType, false, mhKey).Scan(&stateID)
+
+	p.currBatchSize += 2
 	return stateID, err
 }
 
 // PublishStorageNode writes the storage node to the ipfs backing pg datastore and adds secondary indexes in the storage_cids table
-func (p *Publisher) PublishStorageNode(node Node, stateID int64) error {
+func (p *Publisher) PublishStorageNode(node Node, stateID int64, tx *sqlx.Tx) error {
 	var storageKey string
 	if !bytes.Equal(node.Key.Bytes(), nullHash.Bytes()) {
 		storageKey = node.Key.Hex()
 	}
-	tx, err := p.db.Beginx()
-	if err != nil {
-		return err
-	}
+
 	defer func() {
-		if p := recover(); p != nil {
+		if rec := recover(); rec != nil {
 			shared.Rollback(tx)
-			panic(p)
-		} else if err != nil {
-			shared.Rollback(tx)
-		} else {
-			err = tx.Commit()
+			panic(rec)
 		}
 	}()
-	storageCIDStr, err := shared.PublishRaw(tx, ipld.MEthStorageTrie, multihash.KECCAK_256, node.Value)
+
+	storageCIDStr, mhKey, err := shared.PublishRaw(tx, ipld.MEthStorageTrie, multihash.KECCAK_256, node.Value)
 	if err != nil {
 		return err
 	}
-	mhKey, _ := shared.MultihashKeyFromCIDString(storageCIDStr)
-	_, err = tx.Exec(`INSERT INTO eth.storage_cids (state_id, storage_leaf_key, cid, storage_path, node_type, diff, mh_key) VALUES ($1, $2, $3, $4, $5, $6, $7) 
- 							  ON CONFLICT (state_id, storage_path) DO NOTHING`,
+
+	_, err = tx.Exec(`INSERT INTO eth.storage_cids (state_id, storage_leaf_key, cid, storage_path, node_type, diff, mh_key) VALUES ($1, $2, $3, $4, $5, $6, $7)
+                              	ON CONFLICT (state_id, storage_path) DO UPDATE SET (storage_leaf_key, cid, node_type, diff, mh_key) = ($2, $3, $5, $6, $7)`,
 		stateID, storageKey, storageCIDStr, node.Path, node.NodeType, false, mhKey)
-	return err
+	if err != nil {
+		return err
+	}
+
+	p.currBatchSize += 2
+	return nil
 }
 
 // PublishCode writes code to the ipfs backing pg datastore
-func (p *Publisher) PublishCode(code []byte) error {
+func (p *Publisher) PublishCode(code []byte, tx *sqlx.Tx) error {
 	// no codec for code, doesn't matter though since blockstore key is multihash-derived
-	return p.publishRaw(ipld.MEthStorageTrie, multihash.KECCAK_256, code)
+	return p.publishRaw(ipld.MEthStorageTrie, multihash.KECCAK_256, code, tx)
 }
 
-func (p *Publisher) publishRaw(codec, mh uint64, raw []byte) error {
+func (p *Publisher) publishRaw(codec, mh uint64, raw []byte, tx *sqlx.Tx) error {
 	c, err := ipld.RawdataToCid(codec, raw, mh)
 	if err != nil {
 		return err
 	}
+
 	dbKey := dshelp.MultihashToDsKey(c.Hash())
 	prefixedKey := blockstore.BlockPrefix.String() + dbKey.String()
-	_, err = p.db.Exec(`INSERT INTO public.blocks (key, data) VALUES ($1, $2) ON CONFLICT (key) DO NOTHING`, prefixedKey, raw)
-	return err
+	_, err = tx.Exec(`INSERT INTO public.blocks (key, data) VALUES ($1, $2) ON CONFLICT (key) DO NOTHING`, prefixedKey, raw)
+	if err != nil {
+		return err
+	}
+
+	p.currBatchSize++
+	return nil
+}
+
+func (p *Publisher) checkBatchSize(tx *sqlx.Tx, maxBatchSize uint) (*sqlx.Tx, error) {
+	var err error
+	// maximum batch size reached, commit the current transaction and begin a new transaction.
+	if maxBatchSize <= p.currBatchSize {
+		if err = tx.Commit(); err != nil {
+			return nil, err
+		}
+
+		tx, err = p.db.Beginx()
+		if err != nil {
+			return nil, err
+		}
+
+		p.currBatchSize = 0
+	}
+
+	return tx, nil
 }
