f83ab82424bafaa12cf2e0c3a9b7bc14c81aa75a
i-norden <iansnorden@gmail.com>
2023-04-11 10:03:27 -0500

    use types/models exported from vdb geth

--- pkg/types/publisher.go
+++ pkg/types/publisher.go
@@ -3,15 +3,19 @@ package types
 import (
 	"math/big"
 
+	"github.com/ethereum/go-ethereum/statediff/indexer/models"
+	"github.com/ipfs/go-cid"
+
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 )
 
 type Publisher interface {
 	PublishHeader(header *types.Header) error
-	PublishStateNode(node *Node, headerID string, height *big.Int, tx Tx) error
-	PublishStorageNode(node *Node, headerID string, height *big.Int, statePath []byte, tx Tx) error
+	PublishStateLeafNode(node *models.StateNodeModel, tx Tx) error
+	PublishStorageLeafNode(node *models.StorageNodeModel, tx Tx) error
 	PublishCode(height *big.Int, codeHash common.Hash, codeBytes []byte, tx Tx) error
+	PublishIPLD(c cid.Cid, raw []byte, height *big.Int, tx Tx) (string, error)
 	BeginTx() (Tx, error)
 	PrepareTxForBatch(tx Tx, batchSize uint) (Tx, error)
 }
